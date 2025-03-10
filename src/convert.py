0076b441a1e749045412e232a02145384f53f37f
Dmitriy Fishman <fishman.code@gmail.com>
2025-03-10 14:46:32 +0300

    Updated

--- src/convert.py
+++ src/convert.py
@@ -2,16 +2,13 @@ from weasyprint import HTML
 from pathlib import Path
 
 def convert_html_to_pdf():
-    # Получаем абсолютный путь к текущей директории
     current_dir = Path(__file__).parent
     
-    # Пути к входному HTML и выходному PDF
     html_path = current_dir / 'template.html'
     pdf_path = current_dir / 'output.pdf'
     
-    # Конвертируем HTML в PDF
     HTML(html_path.as_uri()).write_pdf(pdf_path)
 
 if __name__ == "__main__":
     convert_html_to_pdf()
-    print("PDF успешно создан в src/output.pdf")
+    print("PDF successfully created at src/output.pdf")
